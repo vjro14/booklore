ee55ba26842e7fe22864d09d7c9196235493415d
vjro14 <vjro_bmw@hotmail.com>
2018-11-21 11:52:51 -0600

    logout

--- app/src/main/java/sv/edu/catolica/booklore/Dashboard.java
+++ app/src/main/java/sv/edu/catolica/booklore/Dashboard.java
@@ -126,14 +126,13 @@ public class Dashboard extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            System.exit(0);
+            session.setPassword("");
+            session.setUserName("");
+            finishAffinity();
         }
 
         return super.onOptionsItemSelected(item);
